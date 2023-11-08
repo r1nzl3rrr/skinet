71e86486a4a927883303d04b43b1c0c9df0101da
r1nzl3rrr <khanhle9344@gmail.com>
2023-11-09 02:03:31 +0700

    Section 2

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,4 +1,5 @@
 ﻿using Core.Entities;
+using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -9,24 +10,36 @@ namespace API.Controllers
     [Route("api/[controller]")]
     public class ProductsController : ControllerBase
     {
-        private readonly StoreContext _context;
+        private readonly IProductRepository _repo;
 
-        public ProductsController(StoreContext context)
+        public ProductsController(IProductRepository repo)
         {
-            _context = context;
+            _repo = repo;
         }
         [HttpGet]
-        public async Task<ActionResult<ICollection<Product>>>GetProducts()
+        public async Task<ActionResult<IList<Product>>>GetProducts()
         {
-            var products = await _context.Products.ToListAsync();
-            return products;
+            var products = await _repo.GetProductsAsync();
+            return Ok(products);
         }
 
         [HttpGet("{id}")]
         public async Task<ActionResult<Product>> GetProduct(int id)
         {
-            var product = await _context.Products.FindAsync(id);
-            return product;
+            return await _repo.GetProductByIdAsync(id);
         }
+
+        [HttpGet("brands")]
+        public async Task<ActionResult<IReadOnlyList<ProductBrand>>> GetProductBrands()
+        {
+            return Ok(await _repo.GetProductBrandsAsync());
+        }
+
+        [HttpGet("types")]
+        public async Task<ActionResult<IReadOnlyList<ProductType>>> GetProductTypes()
+        {
+            return Ok(await _repo.GetProductTypesAsync());
+        }
+
     }
 }
---
+++ Infrastructure/Data/StoreContextSeed.cs
@@ -0,0 +1,60 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Text.Json;
+
+namespace Infrastructure.Data
+{
+    public class StoreContextSeed
+    {
+        public static async Task SeedAsync(StoreContext context)
+        {
+            if (!context.ProductBrands.Any())
+            {
+
+                using var transaction = context.Database.BeginTransaction();
+                var brandsData = File.ReadAllText("../Infrastructure/Data/SeedData/brands.json");
+                var brands = JsonSerializer.Deserialize<List<ProductBrand>>(brandsData); ;
+
+                foreach (var item in brands)
+                {
+                    context.ProductBrands.Add(item);
+                }
+                context.Database.ExecuteSqlRaw("SET IDENTITY_INSERT ProductBrands ON");
+                await context.SaveChangesAsync();
+                context.Database.ExecuteSqlRaw("SET IDENTITY_INSERT ProductBrands OFF");
+                transaction.Commit();
+
+            }
+
+            if (!context.ProductTypes.Any())
+            {
+                using var transaction = context.Database.BeginTransaction();
+                var typesdata = File.ReadAllText("../Infrastructure/Data/SeedData/types.json");
+                var types = JsonSerializer.Deserialize<List<ProductType>>(typesdata);
+
+                foreach (var type in types)
+                {
+                    context.ProductTypes.Add(type);
+                }
+                context.Database.ExecuteSqlRaw("SET IDENTITY_INSERT ProductTypes ON");
+                await context.SaveChangesAsync();
+                context.Database.ExecuteSqlRaw("SET IDENTITY_INSERT ProductTypes OFF");
+                transaction.Commit();
+            }
+
+            if (!context.Products.Any())
+            {
+                var productdata = File.ReadAllText("../Infrastructure/Data/SeedData/products.json");
+                var products = JsonSerializer.Deserialize<List<Product>>(productdata);
+
+                foreach (var pro in products)
+                {
+                    context.Products.Add(pro);
+                }
+
+                await context.SaveChangesAsync();
+            }
+        }
+    }
+    
+}

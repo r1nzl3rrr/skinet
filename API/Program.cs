b6bd8357ef11f35fcde9753516d452a534443d04
r1nzl3rrr <khanhle9344@gmail.com>
2023-11-11 17:56:42 +0700

    section 4

--- API/Program.cs
+++ API/Program.cs
@@ -15,6 +15,8 @@ builder.Services.AddDbContext<StoreContext>(opt =>
     opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 var app = builder.Build();
 
@@ -25,7 +27,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+app.UseStaticFiles();
 
 app.UseAuthorization();
 
--- Core/Entities/Product.cs
+++ Core/Entities/Product.cs
@@ -9,6 +9,6 @@
         public ProductType ProductType { get; set; }
         public int ProductTypeId { get; set; }
         public ProductBrand ProductBrand { get; set; }
-        public int ProductBrandId { get; set;}
+        public int ProductBrandId { get; set; }
     }
 }
